2374fb7b2ecf476ad40df0b36a697a9129d971cd
Andy <aparry@redventures.com>
2016-12-06 22:29:08 -0500

    day 6

---
+++ 2016/day6/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	// open input
+	f, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	// initialize map for saving letters map[COLUMN]map[LETTER]COUNT
+	colChars := make(map[int]map[string]int)
+
+	// initialize []byte to hold message
+	message := make([]byte, 8)
+
+	// loop through each line
+	line := bufio.NewScanner(f)
+	for line.Scan() {
+
+		// loop through each character on each line
+		characters := bufio.NewScanner(strings.NewReader(line.Text()))
+		characters.Split(bufio.ScanRunes)
+		col := 0
+		for characters.Scan() {
+
+			letter := characters.Text()
+
+			// if the column key isn't created, create it
+			if colChars[col] == nil {
+				colChars[col] = make(map[string]int)
+			}
+
+			// if letter isn't in letters map create it and initialize count
+			if colChars[col][letter] == 0 {
+				colChars[col][letter] = 1
+			} else {
+				// increment count if it exists
+				colChars[col][letter]++
+			}
+
+			// add to message if letter has highest count
+			if colChars[col][letter] > colChars[col][string(message[col])] {
+				message[col] = []byte(letter)[0]
+			}
+
+			// increment col
+			col++
+		}
+	}
+
+	fmt.Println("Part 1 message:", string(message))
+	getMessageLowestCount(colChars)
+}
+
+func getMessageLowestCount(c map[int]map[string]int) {
+
+	// initialize message
+	message := make([]byte, 8)
+
+	for column, letters := range c {
+
+		var lowest string
+
+		for letter, count := range letters {
+
+			if lowest == "" {
+				lowest = letter
+			}
+
+			if count < letters[lowest] {
+				lowest = letter
+			}
+
+		}
+		message[column] = []byte(lowest)[0]
+	}
+
+	fmt.Println("Part 2 message:", string(message))
+
+}
